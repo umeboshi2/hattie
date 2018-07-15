b60773c3b2d0dfbc47ae0d70c71eab27602b5604
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-15 17:19:12 -0500

    fix missing quote

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     include_package_data=True,
     keywords='hattie',
     name='hattie',
-    packages=find_packages(include=['hattie', 'hattie.*]),
+    packages=find_packages(include=['hattie', 'hattie.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
