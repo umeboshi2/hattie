c5b333730a368b5b8add3ad2f2de12991cab67cb
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-15 17:12:04 -0500

    Bump version: 0.2.8 → 0.2.9

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/umeboshi2/hattie',
-    version='0.2.8',
+    version='0.2.9',
     zip_safe=False,
 )
