571329c26ba8712b2af2ce07811b72a7faf708f2
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-31 20:52:32 -0500

    added requirements

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'feedparser',
+    'requests',
+    'robobrowser',
+    'transaction',
 ]
 
 setup_requirements = ['pytest-runner', ]
